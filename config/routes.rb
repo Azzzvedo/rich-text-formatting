04cb5b40e85ba8e033978c878f0b8594cdfd0827
Ian Wiggins <wiggins.ian.r@gmail.com>
2022-11-24 21:14:38 +0000

    add crud functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/show'
-  get 'posts/index'
-  get 'posts/edit'
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
